d43b6377d9717e8a849616cd157e025ad55f3377
Akayn-team <stasmolchanov7@gmail.com>
2023-07-16 20:35:01 +0300

    Добавьте файлы проекта.

---
+++ MainForm.h
@@ -0,0 +1,308 @@
+#pragma once
+#include <chrono>
+#include <thread>
+namespace AccountManager {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+	public: bool isclose = false;
+	private: System::Windows::Forms::TabControl^ tabControl1;
+	public:
+	private: System::Windows::Forms::TabPage^ tabPage1;
+	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::PictureBox^ LefTo;
+	private: System::Windows::Forms::PictureBox^ ToLow;
+	private: System::Windows::Forms::Timer^ ToLowRepeat;
+	private: System::Windows::Forms::PictureBox^ ToBackgr;
+	private: System::Windows::Forms::PictureBox^ Clo;
+	private: System::Windows::Forms::PictureBox^ HorLin;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Timer^ ToBkgRepeat;
+
+
+	private: System::ComponentModel::IContainer^ components;
+
+
+
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainForm::typeid));
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->LefTo = (gcnew System::Windows::Forms::PictureBox());
+			this->ToLow = (gcnew System::Windows::Forms::PictureBox());
+			this->ToLowRepeat = (gcnew System::Windows::Forms::Timer(this->components));
+			this->ToBackgr = (gcnew System::Windows::Forms::PictureBox());
+			this->Clo = (gcnew System::Windows::Forms::PictureBox());
+			this->HorLin = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->ToBkgRepeat = (gcnew System::Windows::Forms::Timer(this->components));
+			this->tabControl1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->LefTo))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ToLow))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ToBackgr))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Clo))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->HorLin))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Alignment = System::Windows::Forms::TabAlignment::Bottom;
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Location = System::Drawing::Point(-4, 34);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->Padding = System::Drawing::Point(0, 0);
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(1061, 544);
+			this->tabControl1->TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"tabPage1.BackgroundImage")));
+			this->tabPage1->Location = System::Drawing::Point(4, 4);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(1053, 518);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"tabPage1";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Location = System::Drawing::Point(4, 4);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(1053, 518);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"tabPage2";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// LefTo
+			// 
+			this->LefTo->BackColor = System::Drawing::Color::Transparent;
+			this->LefTo->Location = System::Drawing::Point(20, 2);
+			this->LefTo->Name = L"LefTo";
+			this->LefTo->Size = System::Drawing::Size(198, 27);
+			this->LefTo->TabIndex = 1;
+			this->LefTo->TabStop = false;
+			// 
+			// ToLow
+			// 
+			this->ToLow->BackColor = System::Drawing::Color::Transparent;
+			this->ToLow->Location = System::Drawing::Point(896, 11);
+			this->ToLow->Name = L"ToLow";
+			this->ToLow->Size = System::Drawing::Size(43, 9);
+			this->ToLow->TabIndex = 1;
+			this->ToLow->TabStop = false;
+			this->ToLow->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MainForm::ToLow_MouseClick);
+			this->ToLow->MouseEnter += gcnew System::EventHandler(this, &MainForm::ToLow_MouseEnter);
+			this->ToLow->MouseLeave += gcnew System::EventHandler(this, &MainForm::ToLow_MouseLeave);
+			// 
+			// ToLowRepeat
+			// 
+			this->ToLowRepeat->Interval = 300;
+			this->ToLowRepeat->Tick += gcnew System::EventHandler(this, &MainForm::ToLowRepeat_Tick);
+			// 
+			// ToBackgr
+			// 
+			this->ToBackgr->BackColor = System::Drawing::Color::Transparent;
+			this->ToBackgr->Location = System::Drawing::Point(945, 11);
+			this->ToBackgr->Name = L"ToBackgr";
+			this->ToBackgr->Size = System::Drawing::Size(43, 9);
+			this->ToBackgr->TabIndex = 1;
+			this->ToBackgr->TabStop = false;
+			this->ToBackgr->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MainForm::ToBackgr_MouseClick);
+			this->ToBackgr->MouseEnter += gcnew System::EventHandler(this, &MainForm::ToBackgr_MouseEnter);
+			this->ToBackgr->MouseLeave += gcnew System::EventHandler(this, &MainForm::ToBackgr_MouseLeave);
+			// 
+			// Clo
+			// 
+			this->Clo->BackColor = System::Drawing::Color::Transparent;
+			this->Clo->Location = System::Drawing::Point(994, 11);
+			this->Clo->Name = L"Clo";
+			this->Clo->Size = System::Drawing::Size(43, 9);
+			this->Clo->TabIndex = 1;
+			this->Clo->TabStop = false;
+			this->Clo->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MainForm::Clo_MouseClick);
+			this->Clo->MouseEnter += gcnew System::EventHandler(this, &MainForm::Clo_MouseEnter);
+			this->Clo->MouseLeave += gcnew System::EventHandler(this, &MainForm::Clo_MouseLeave);
+			// 
+			// HorLin
+			// 
+			this->HorLin->BackColor = System::Drawing::Color::Transparent;
+			this->HorLin->Location = System::Drawing::Point(10, 30);
+			this->HorLin->Name = L"HorLin";
+			this->HorLin->Size = System::Drawing::Size(1030, 2);
+			this->HorLin->TabIndex = 1;
+			this->HorLin->TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(31)), static_cast<System::Int32>(static_cast<System::Byte>(35)),
+				static_cast<System::Int32>(static_cast<System::Byte>(39)));
+			this->pictureBox1->Location = System::Drawing::Point(0, 34);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(1057, 18);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// ToBkgRepeat
+			// 
+			this->ToBkgRepeat->Enabled = true;
+			this->ToBkgRepeat->Interval = 300;
+			this->ToBkgRepeat->Tick += gcnew System::EventHandler(this, &MainForm::ToBkgRepeat_Tick);
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->ClientSize = System::Drawing::Size(1090, 590);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->HorLin);
+			this->Controls->Add(this->Clo);
+			this->Controls->Add(this->ToBackgr);
+			this->Controls->Add(this->ToLow);
+			this->Controls->Add(this->LefTo);
+			this->Controls->Add(this->tabControl1);
+			this->DoubleBuffered = true;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
+			this->tabControl1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->LefTo))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ToLow))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ToBackgr))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Clo))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->HorLin))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	Bitmap^ LefTop = gcnew Bitmap("Resources\\TopPart\\LeftTop.png");
+	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		this->LefTo->Image = LefTop;
+		ToLow->Image = gcnew Bitmap("Resources\\TopPart\\LowSizeNonAct.png");
+		ToBackgr->Image = gcnew Bitmap("Resources\\TopPart\\ToBkgNonAct.png");
+		Clo->Image = gcnew Bitmap("Resources\\TopPart\\CloseNonAct.png");
+		HorLin->Image = gcnew Bitmap("Resources\\TopPart\\HorLin.png");
+		this->ClientSize = System::Drawing::Size(1050, 550);
+			SetRegion();
+			
+	}
+		   void SetRegion()
+		   {
+			   Drawing::Drawing2D::GraphicsPath^ path = gcnew Drawing::Drawing2D::GraphicsPath();
+			   int cornerRadius = 30;
+			   int width = this->Width;
+			   int height = this->Height;
+
+			   path->StartFigure();
+			   path->AddArc(0, 0, cornerRadius, cornerRadius, 180, 90);
+			   path->AddLine(cornerRadius, 0, width - cornerRadius, 0);
+			   path->AddArc(width - cornerRadius, 0, cornerRadius, cornerRadius, 270, 90);
+			   path->AddLine(width, cornerRadius, width, height - cornerRadius);
+			   path->AddArc(width - cornerRadius, height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
+			   path->AddLine(width - cornerRadius, height, cornerRadius, height);
+			   path->AddArc(0, height - cornerRadius, cornerRadius, cornerRadius, 90, 90);
+			   path->AddLine(0, height - cornerRadius, 0, cornerRadius);
+			   path->CloseFigure();
+
+			   this->Region = gcnew Drawing::Region(path);
+		   }
+		   
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+	}
+	private: System::Void ToLow_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
+		ToLow->Image = gcnew Bitmap("Resources\\TopPart\\LowSize.png");
+	}
+private: System::Void ToLow_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
+	ToLow->Image = gcnew Bitmap("Resources\\TopPart\\LowSizeNonAct.png");
+}
+
+private: System::Void ToLow_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+	ToLow->Image = gcnew Bitmap("Resources\\TopPart\\ToLowPress.png");
+	ToLowRepeat->Enabled = true;
+}
+private: System::Void ToLowRepeat_Tick(System::Object^ sender, System::EventArgs^ e) {
+	ToLow->Image = gcnew Bitmap("Resources\\TopPart\\LowSize.png");
+	ToLowRepeat->Enabled = false;
+}
+private: System::Void ToBackgr_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
+	ToBackgr->Image = gcnew Bitmap("Resources\\TopPart\\ToBkg.png");
+}
+private: System::Void ToBackgr_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
+	ToBackgr->Image = gcnew Bitmap("Resources\\TopPart\\ToBkgNonAct.png");
+}
+private: System::Void Clo_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
+	Clo->Image = gcnew Bitmap("Resources\\TopPart\\Close.png");
+}
+private: System::Void Clo_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
+	Clo->Image = gcnew Bitmap("Resources\\TopPart\\CloseNonAct.png");
+}
+private: System::Void Clo_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+	isclose = true;
+}
+private: System::Void ToBackgr_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+	ToBackgr->Image = gcnew Bitmap("Resources\\TopPart\\ToBkgPress.png");
+	ToBkgRepeat->Enabled = true;
+}
+private: System::Void ToBkgRepeat_Tick(System::Object^ sender, System::EventArgs^ e) {
+	ToBackgr->Image = gcnew Bitmap("Resources\\TopPart\\ToBkg.png");
+	ToBkgRepeat->Enabled = false;
+}
+};
+}
---
+++ Start.h
@@ -0,0 +1,277 @@
+#pragma once
+#include <msclr/marshal_cppstd.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+#include <Windows.h>
+#include "MainForm.h"
+namespace AccountManager {
+
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Collections;
+    using namespace System::Windows::Forms;
+    using namespace System::Data;
+    using namespace System::Drawing;
+    using namespace System::Drawing::Imaging;
+    using namespace System::Windows::Forms::VisualStyles;
+    /// <summary>
+    /// —водка дл€ Start
+    /// </summary>
+    public ref class Start : public System::Windows::Forms::Form
+    {
+    private:
+        bool isDragging;
+        Point dragStartPosition;
+    private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+    private: System::Windows::Forms::Timer^ timer1;
+    private: System::Windows::Forms::Label^ label1;
+    private: System::Windows::Forms::Timer^ timer2;
+
+           ref class RoundedPictureBox : public PictureBox
+           {
+           public:
+               RoundedPictureBox()
+               {
+                   this->BackColor = Color::Transparent;
+               }
+
+           protected:
+               virtual void OnResize(EventArgs^ e) override
+               {
+                   __super::OnResize(e);
+                   Drawing::Drawing2D::GraphicsPath^ path = gcnew Drawing::Drawing2D::GraphicsPath();
+                   int cornerRadius = 30;
+                   int width = this->Width;
+                   int height = this->Height;
+                   path->StartFigure();
+                   path->AddArc(0, 0, cornerRadius, cornerRadius, 180, 90);
+                   path->AddLine(cornerRadius, 0, width - cornerRadius, 0);
+                   path->AddArc(width - cornerRadius, 0, cornerRadius, cornerRadius, 270, 90);
+                   path->AddLine(width, cornerRadius, width, height - cornerRadius);
+                   path->AddArc(width - cornerRadius, height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
+                   path->AddLine(width - cornerRadius, height, cornerRadius, height);
+                   path->AddArc(0, height - cornerRadius, cornerRadius, cornerRadius, 90, 90);
+                   path->AddLine(0, height - cornerRadius, 0, cornerRadius);
+                   path->CloseFigure();
+
+                   this->Region = gcnew Drawing::Region(path);
+               }
+           };
+    public:
+        RoundedPictureBox^ pictureBox = gcnew RoundedPictureBox();
+        Start(void)
+        {
+            this->SetStyle(ControlStyles::SupportsTransparentBackColor, true);
+            this->TransparencyKey = Color::FromArgb(1, 1, 1);
+            this->StartPosition = FormStartPosition::CenterScreen;
+            pictureBox->SizeMode = PictureBoxSizeMode::AutoSize;
+            pictureBox->BackColor = Color::Transparent;
+            pictureBox->Location = System::Drawing::Point(0, 0);
+            pictureBox->Name = L"pictureBox1";
+            pictureBox->Size = System::Drawing::Size(1050, 550);
+            pictureBox->TabIndex = 1;
+            pictureBox->TabStop = false;
+            pictureBox->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Start::pictureBox1_Paint);
+            this->Controls->Add(pictureBox);
+            pictureBox->BringToFront();
+            InitializeComponent();
+            this->Opacity = 0;
+            //
+            //TODO: добавьте код конструктора
+            //
+
+        }
+
+    protected:
+        /// <summary>
+        /// ќсвободить все используемые ресурсы.
+        /// </summary>
+        ~Start()
+        {
+            if (components)
+            {
+                delete components;
+            }
+        }
+    private: System::ComponentModel::IContainer^ components;
+    protected:
+
+    private:
+        /// <summary>
+        /// ќб€зательна€ переменна€ конструктора.
+        /// </summary>
+
+
+#pragma region
+        /// <summary>
+        /// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        void InitializeComponent(void)
+        {
+            this->components = (gcnew System::ComponentModel::Container());
+            System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Start::typeid));
+            this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+            this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+            this->label1 = (gcnew System::Windows::Forms::Label());
+            this->timer2 = (gcnew System::Windows::Forms::Timer(this->components));
+            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+            this->SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this->pictureBox1->Location = System::Drawing::Point(9999, 9999);
+            this->pictureBox1->Name = L"pictureBox1";
+            this->pictureBox1->Size = System::Drawing::Size(1050, 550);
+            this->pictureBox1->TabIndex = 1;
+            this->pictureBox1->TabStop = false;
+            this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Start::pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this->timer1->Interval = 15;
+            this->timer1->Tick += gcnew System::EventHandler(this, &Start::timer1_Tick);
+            // 
+            // label1
+            // 
+            this->label1->AutoSize = true;
+            this->label1->Location = System::Drawing::Point(24, 145);
+            this->label1->Name = L"label1";
+            this->label1->Size = System::Drawing::Size(35, 13);
+            this->label1->TabIndex = 3;
+            this->label1->Text = L"label1";
+            // 
+            // timer2
+            // 
+            this->timer2->Enabled = true;
+            this->timer2->Tick += gcnew System::EventHandler(this, &Start::timer2_Tick);
+            // 
+            // Start
+            // 
+            this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+            this->BackColor = System::Drawing::Color::Black;
+            this->ClientSize = System::Drawing::Size(1050, 550);
+            this->Controls->Add(this->pictureBox1);
+            this->Controls->Add(this->label1);
+            this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+            this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+            this->Name = L"Start";
+            this->Text = L"Start";
+            this->Load += gcnew System::EventHandler(this, &Start::Start_Load);
+            this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Start::Start_Paint);
+            this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &Start::Start_MouseDown);
+            this->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &Start::Start_MouseMove);
+            this->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &Start::Start_MouseUp);
+            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+            this->ResumeLayout(false);
+            this->PerformLayout();
+
+        }
+#pragma endregion
+        bool isHovered = false;
+        MainForm^ Begin = gcnew MainForm;
+    private:
+        void Start_Load(System::Object^ sender, System::EventArgs^ e)
+        {
+
+            this->timer1->Enabled = true;
+            Begin->Opacity = 0;
+            SetRegion();
+
+        }
+        void SetRegion()
+        {
+            Drawing::Drawing2D::GraphicsPath^ path = gcnew Drawing::Drawing2D::GraphicsPath();
+            int cornerRadius = 30;
+            int width = this->Width;
+            int height = this->Height;
+
+            path->StartFigure();
+            path->AddArc(0, 0, cornerRadius, cornerRadius, 180, 90);
+            path->AddLine(cornerRadius, 0, width - cornerRadius, 0);
+            path->AddArc(width - cornerRadius, 0, cornerRadius, cornerRadius, 270, 90);
+            path->AddLine(width, cornerRadius, width, height - cornerRadius);
+            path->AddArc(width - cornerRadius, height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
+            path->AddLine(width - cornerRadius, height, cornerRadius, height);
+            path->AddArc(0, height - cornerRadius, cornerRadius, cornerRadius, 90, 90);
+            path->AddLine(0, height - cornerRadius, 0, cornerRadius);
+            path->CloseFigure();
+
+            this->Region = gcnew Drawing::Region(path);
+        }
+
+        void Start_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+        {
+            if (e->Button == System::Windows::Forms::MouseButtons::Left && e->Y < 50)
+            {
+                isDragging = true;
+                dragStartPosition = Point(e->X, e->Y);
+            }
+        }
+
+        void Start_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+        {
+            if (isDragging)
+            {
+                Point pointDifference = Point(e->X - dragStartPosition.X, e->Y - dragStartPosition.Y);
+                this->Location = Point(this->Location.X + pointDifference.X, this->Location.Y + pointDifference.Y);
+            }
+        }
+        void Start_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
+        {
+            Drawing::Drawing2D::GraphicsPath^ path = gcnew Drawing::Drawing2D::GraphicsPath();
+            path->AddRectangle(this->ClientRectangle);
+            this->Region = gcnew Drawing::Region(path);
+
+            e->Graphics->Clear(Color::FromArgb(1, 1, 1));
+
+            // ќтрисовка фона формы здесь
+            // ...
+        }
+        void Start_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+        {
+            isDragging = false;
+        }
+    private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+        
+    }
+    private: System::Void pictureBox1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+
+    }
+    private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+    }
+    
+           int i = 0;
+    private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+        this->label1->Text = Convert::ToString(this->timer1->Interval);
+        if (i < 179) { Bitmap^ alphaImage = gcnew Bitmap("Resources\\Animation\\PngAnim_" + i + ".png"); pictureBox->Image = alphaImage; }
+        i++;
+        if (i == 165) {
+            
+            Begin->Show();
+            Begin->Location = this->Location;
+        }
+        if (i == 179) {
+            this->Opacity = 0;
+            this->Hide();
+        }
+        if (i == 179) { Begin->Opacity = 1; this->timer1->Enabled = false;
+        }
+        else if (i == 1) { this->Opacity = 1; }
+        if (i == 69) {PlaySound(L"Str.wav", NULL, SND_FILENAME | SND_ASYNC); }
+
+    }
+    private: System::Void timer2_Tick(System::Object^ sender, System::EventArgs^ e) {
+        if (Begin->isclose) {
+            timer1->Enabled = false;
+            timer2->Enabled = false;
+            Begin->Hide();
+            Begin->Close();
+            System::Environment::Exit(0);
+            Application::Exit();
+    }
+    }
+};
+}
